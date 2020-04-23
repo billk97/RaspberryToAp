e52f0a554be4b54f3912af3007e4055e4ffe27da
billk97 <bill1897@yahoo.gr>
2020-04-23 14:52:43 +0300

    first atempt

--- installer.py
+++ installer.py
@@ -1,15 +1,7 @@
 import os, subprocess
 
 print("Starting script")
-
-def is_tool(name):
-    try:
-        devnull = open(os.devnull)
-        subprocess.Popen([name], stdout=devnull, stderr=devnull).communicate()
-    except OSError as e:
-        if e.errno = os.errno.ENOENT:
-            return False
-    return True
-
-is_tool(git)
-is_tool(tmux)
+password = input("Enter password: ")
+print("installing hostapt")
+os.system("sudo apt install hostapt | y" )
+os.system("sudo apt install dnsmasq | y" )
