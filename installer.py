765db79fc9c5436d3869aba9f23baab866bfd48f
billk97 <bill1897@yahoo.gr>
2020-04-26 23:22:45 +0300

    small changes

--- installer.py
+++ installer.py
@@ -3,8 +3,8 @@ import os, subprocess
 print("Starting script")
 static_ip = input("Enter static ip: ")
 channel = 7
-ssid = "temp"
-password = "temp"
+ssid = input("ssidName: ")
+password = input("password")
 print("installing hostapt")
 os.system("sudo apt install hostapd")
 print("installing dnsmasq")
@@ -18,13 +18,13 @@ file_dhcpcd.write("\n interface wla0 \n"
            "denyinterfaces wlan0")
 file_dhcpcd.close()
 
-file_dnsmsq = open("/etc/dnsmsq.conf", "r")
-file_safe = open("/etc/dnsmsq.conf.orig", "w")
+file_dnsmsq = open("/etc/dnsmasq.conf", "r")
+file_safe = open("/etc/dnsmasq.conf.orig", "w")
 file_safe = file_dnsmsq
 file_safe.close()
 file_dnsmsq.close()
-open("/etc/dnsmsq.conf", "w").close()
-dnsmsq = open("/etc/dnsmsq.conf", "w")
+open("/etc/dnsmasq.conf", "w").close()
+dnsmsq = open("/etc/dnsmasq.conf", "w")
 dnsmsq.write("interface=wlan0 \n"
              "dhcp-range=192.168.2.10, 192.168.2.40, 255.255.255.0,24h")
 dnsmsq.close()
@@ -44,4 +44,17 @@ file_hostapd.write("interface=wlan0 \n "
                     "ssid=" +ssid+" \n"
                     "wpa_passphrase="+password)
 file_hostapd.close()
-file_default_hostapd = open("/etc/default/hostapd" "rw")
+#file_default_hostapd = open("/etc/default/hostapd" "rw")
+#sudo nano /etc/sysctl.conf
+##net.ipv4.ip_forward=1 -> net.ipv4.ip_forward=1
+# sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+# iptables-restore < /etc/iptables.ipv4.nat
+# sudo apt-get install bridge-utils
+# sudo brctl addbr br0
+# sudo brctl addif br0 eth0
+# sudo nano /etc/network/interfaces
+#auto br0
+#iface br0 inet manual
+#bridge_ports eth0 wlan0
+#sudo reboot
